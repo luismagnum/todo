47a322e8e8639a3ddbfdcfdf34751267c202dde1
luismagnum <luismagnum1@gmail.com>
2024-06-12 22:32:18 -0300

    favicon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -52,7 +52,7 @@ const Todo = () => {
               <h1 className='text-2xl font-semibold ml-2'>TAREAS</h1>
             </div>
             <div className='flex items-center mb-6 bg-gray-700 rounded-full overflow-x-hidden'>
-              <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-12 pl-4 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task'/>
+              <input ref={inputRef} className='bg-transparent border-0 outline-none text-white flex-1 h-12 pl-4 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task'/>
               <button onClick={add} className='border-none rounded-full bg-orange-600 px-6 h-12 text-white text-lg font-medium'>ADD</button>
             </div>
             <div className='space-y-4'>
